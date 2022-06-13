275799990615809fbe1e9b9c3c53c5355164aee2
laxmi13579 <laxmi.yadav370515@gmail.com>
2022-06-13 23:11:27 +0545

    fetch posts, users and comments from api useing useEffect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [resourceType, setResourceType] = useState('posts');
+  const [data,setData] = useState([]);
+
+  // call on render
+  useEffect(()=>{
+    console.log('render')
+  })
+  //call on mount
+  useEffect(()=>{
+    console.log('on mount')
+  },[])
+  //call on resourceType state change
+  useEffect(()=>{
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+    .then(response => response.json())
+    .then(json => setData(json))
+  },[resourceType])
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>{resourceType}</p>
+      <button onClick={()=>setResourceType('posts')}>Posts</button>
+      <button onClick={()=>setResourceType('users')}>Users</button>
+      <button onClick={()=>setResourceType('comments')}>Comments</button>
+      {data.map(data=>{
+        return <pre>{JSON.stringify(data)}</pre>
+      })}
+    </>
   );
 }
 
